47b64f7fc3551fbcb602b0a536f907689fd7b7d5
ShestakovSergey <s21021977@gmail.com>
2022-03-17 20:10:52 +0300

    Lesson_1

---
+++ Course/Base/Lesson_1.py
@@ -0,0 +1,13 @@
+print("Hello world!")
+print("Hello \" world!")
+print('Hello "\\" world!')
+print('2 + 3')
+print(2 + 3)
+print("Hello,", "Alex")
+print("Hello,", "Alex", "and", "Michil", sep='   ')
+
+print("Hello,\nAlex")
+
+print('')
+
+print("I take a course in Python.\nI learn Python in the course: 'Python Programming from Zero to Guru'")
