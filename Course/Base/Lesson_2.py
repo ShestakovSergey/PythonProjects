a21422f8a88ceca6ccf912b16a682ba359bb04ee
ShestakovSergey <s21021977@gmail.com>
2022-03-22 21:55:45 +0300

    created lesson_2

---
+++ Course/Base/Lesson_2.py
@@ -0,0 +1,43 @@
+# integer
+age = 25
+print('Возраст:', age)
+
+# float
+
+temp = 5.9  # float
+print('Температура:', temp, 'градусов')
+
+# string
+
+username = 'Serg'
+print('Имя пользователя:', username)
+
+# bool
+
+isexists = True
+print('Существует', isexists)
+isexists = False
+print('Существует', isexists)
+
+# Type of variable
+
+print('Тип переменной age:', type(age))
+print('Тип переменной temp:', type(temp))
+print('Тип переменной username:', type(username))
+print('Тип переменной isexists:', type(isexists))
+print('Тип 10:', type(10))
+
+# Homework
+
+numb = 10
+print ('Занчение переменной numb равно:', numb)
+numb = 15
+print ('Занчение переменной numb равно:', numb)
+firstname = "Shestakov"
+print ('Занчение переменной firstname равно:', firstname)
+firstname = "Trishina"
+print ('Занчение переменной firstname равно:', firstname)
+smoking = True
+print ('Занчение переменной smoking равно:', smoking)
+smoking = False
+print ('Занчение переменной smoking равно:', smoking)
