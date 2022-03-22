2a9b1927bbe5ccff66c4e1b4eace1982dfdbf692
ShestakovSergey <s21021977@gmail.com>
2022-03-22 14:09:46 +0300

    added 1 line Lesson_1

--- Course/Base/Lesson_1.py
+++ Course/Base/Lesson_1.py
@@ -9,5 +9,6 @@ print("Hello,", "Alex", "and", "Michil", sep='   ')
 print("Hello,\nAlex")
 
 print('')
+print('')
 
 print("I take a course in Python.\nI learn Python in the course: 'Python Programming from Zero to Guru'")
