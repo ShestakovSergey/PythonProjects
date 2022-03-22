436703acc43d13b539c7df250f90dbd31bd64ece
ShestakovSergey <63974110+ShestakovSergey@users.noreply.github.com>
2022-03-22 15:38:03 +0300

    Update Lesson_1.py

--- Course/Base/Lesson_1.py
+++ Course/Base/Lesson_1.py
@@ -9,6 +9,5 @@ print("Hello,", "Alex", "and", "Michil", sep='   ')
 print("Hello,\nAlex")
 
 print('')
-print('')
 
 print("I take a course in Python.\nI learn Python in the course: 'Python Programming from Zero to Guru'")
