5af832eab797ea35ef45bb54a38b3b05b58ff21c
ShestakovSergey <s21021977@gmail.com>
2022-03-27 21:30:39 +0300

    created Lesson_3

---
+++ Course/Base/Lesson_3.py
@@ -0,0 +1,53 @@
+#Инициализация переменной типа int
+age = 25 #Пресваивание переменной значения
+print('Возраст:', age) #Вывод переменной
+
+# float
+
+temp = 5.9  #Инициализация переменной типа float
+print('Температура:', temp, 'градусов')
+
+#Инициализация переменной типа string
+
+username = 'Serg'
+print('Имя пользователя:', username)
+
+#Инициализация переменной типа bool
+
+isexists = True
+print('Существует', isexists)
+isexists = False
+print('Существует', isexists)
+
+# Type of variable
+
+print('Тип переменной age:', type(age))
+print('Тип переменной temp:', type(temp))
+print('Тип переменной username:', type(username))
+print('Тип переменной isexists:', type(isexists))
+print('Тип 10:', type(10))
+
+#Homework
+
+numb = 10
+print ('Занчение переменной numb равно:', numb)
+numb = 15
+print ('Занчение переменной numb равно:', numb)
+firstname = "Shestakov"
+print ('Занчение переменной firstname равно:', firstname)
+firstname = "Trishina"
+print ('Занчение переменной firstname равно:', firstname)
+smoking = True
+print ('Занчение переменной smoking равно:', smoking)
+smoking = False
+print ('Занчение переменной smoking равно:', smoking)
+
+#ended
+
+#Создание многострочного комментария
+text = '''Первая строка
+Вторая строка
+Третья строка'''
+
+#Вывод многострочного комментария
+print(text)
