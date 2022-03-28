d63ba7344e522f7ab6bc805318decc3ed07be417
ShestakovSergey <s21021977@gmail.com>
2022-03-28 22:43:55 +0300

    created Lesson_4

---
+++ Course/Base/Lesson_4.py
@@ -0,0 +1,47 @@
+x = 13
+y = 4
+print("x = ", x)
+print("y = ", y)
+
+#Основные математические операции
+
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", x / y)
+print("x % y =", x % y)
+print("x // y =", x // y)
+print("x ** y =", x ** y)
+print("2 ** 3 =", 2 ** 3)
+
+#Битовые операции
+
+x = 5
+y = 6
+z = -5
+print("x =", x, "=", bin(x))
+print("y =", y, "=", bin(y))
+print("z =", z, "=", bin(z))
+print("------------------------------")
+print("x | y =", x | y)
+print("x & y =", x & y)
+print("x ^ y =", x ^ y)
+print("------------------------------")
+print("x << 1 =", bin(x<<1))
+print("x >> 1 =", bin(x>>1))
+
+#Homework
+print("-------------------------HOMEWORK------------------------------")
+n = 9
+m = 2
+print(n + m)
+print(n - m)
+print(n * m)
+print(n / m)
+print(n % m)
+print(n // m)
+print(n ** m)
+print(((15 * 10 - 20) / 2) + 14 * 10 + (-45))
+
+t = bin(n)
+print(t)
