2ae572021e73d194b6b11228d559c09468b4acbe
ShestakovSergey <s21021977@gmail.com>
2022-04-03 23:42:54 +0300

    added print HOMEWORK

--- Course/Base/Lesson_5.py
+++ Course/Base/Lesson_5.py
@@ -23,7 +23,7 @@ print("7 <= 7 =", 7 <= 7)
 print("x and b1 or (x > 10) =", x and b1 or (x > 10))
 print("x > 10 or y < 7 =", x > 10 or y < 7)
 
-#Homework
+print("------------------HOMEWORK-------------------")
 
 print("True and (True or (False and True or False) and True or True != False) =", True and (True or (False and True or False) and True or True != False))
 print("15 > 20 or (5 < 7 and 8 > 12 or 12 >= 12 and 15 < 18 =", 15 > 20 or (5 < 7 and 8 > 12 or 12 >= 12 and 15 < 18))
