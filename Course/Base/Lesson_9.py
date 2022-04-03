ac71ab0681baeee665ae8f6081f0a9d83a812fa8
ShestakovSergey <s21021977@gmail.com>
2022-04-03 23:25:27 +0300

    Created Lesson_9

--- Course/Base/Lesson_9.py
+++ Course/Base/Lesson_9.py
@@ -1,6 +1,6 @@
 list = []
 print(list)
-
+'''
 list = ['h', 'e', 'l', 'l', 'o']
 print(list[1])
 print(list[0])
@@ -47,3 +47,17 @@ while i < len(prices):
     i += 1
 print("MIN value of prices:", minimum)
 print("MAX value of prices:", maximum)
+'''
+print("------------------HOMEWORK------------------------")
+
+fruits = ['apple', 'apricots', 'avocado', 'banano', 'blackberries', 'blackcurrant', 'carambola']
+t = 0
+while t < len(fruits):
+    print(t, "-", fruits[t])
+    t += 1
+print("Input index of element list fruits:")
+y = int(input())
+if y < 0 or y >= len(fruits):
+    print("Element isn't likewise index")
+    exit(0)
+print(fruits[y])
