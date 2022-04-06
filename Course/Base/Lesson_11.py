b0e8b278cd5d0b1e0b79e0d354ea40a6018eea13
ShestakovSergey <s21021977@gmail.com>
2022-04-06 22:32:43 +0300

    created Lesson_11

---
+++ Course/Base/Lesson_11.py
@@ -0,0 +1,16 @@
+import random
+
+myset = set()
+print(myset)
+
+myset = set("Python")
+print(myset)
+
+myset = {'1', 2, 3, 1, '1'}
+print(myset)
+
+list = [int(i * random.random() * 10) for i in range(0, 20) ]
+print(list)
+myset = set(list)
+print(myset)
+
