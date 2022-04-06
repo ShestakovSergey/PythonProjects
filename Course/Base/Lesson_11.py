a636ace8595ef5b1c2e0579dc6137d6a513eec62
ShestakovSergey <s21021977@gmail.com>
2022-04-06 22:52:37 +0300

    change Lesson_11

--- Course/Base/Lesson_11.py
+++ Course/Base/Lesson_11.py
@@ -9,7 +9,7 @@ print(myset)
 myset = {'1', 2, 3, 1, '1'}
 print(myset)
 
-list = [int(i * random.random() * 10) for i in range(0, 20) ]
+list = [int(i * random.random() * 10) for i in range(0, 10)]
 print(list)
 myset = set(list)
 print(myset)
