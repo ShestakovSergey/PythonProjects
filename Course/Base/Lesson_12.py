4522fe1beac228ae7232e7026a611edd7ef2c4ca
ShestakovSergey <s21021977@gmail.com>
2022-04-08 00:12:59 +0300

    created Lesson_12

---
+++ Course/Base/Lesson_12.py
@@ -0,0 +1,14 @@
+mytuple = tuple()
+print(mytuple)
+
+#Создание кортежа из одного элемента
+mytuple = (1,)
+print(mytuple)
+
+mytuple = (1, '3', '5')
+print(mytuple)
+print(mytuple[1])
+
+mytuple = tuple('Python')
+print(mytuple)
+
